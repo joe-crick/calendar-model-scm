9c92a6f976d95e91d3c70c52faf13c77bfdd4c27
Joe Crick <joe@josephcrick.com>
2017-04-15 11:33:55 +0200

    Baseline: Simple time utilities

---
+++ time_utils.scm
@@ -0,0 +1,28 @@
+
+(define (get-twelve-hour-time slot)
+  (if (<= slot 12) 12 (- slot 12)))
+
+(define (get-time-period slot)
+  (if (and (< slot 12) (>= slot 0) ) "AM" "PM"))
+
+(define (get-hour-from-time time)
+ (string->number (if (> (string-length time) 4)
+                     (substring time 0 2)
+                     (substring time 0 1))))
+
+(define (get-display-minutes time)
+  (if (> (string-length time) 4)
+                     (substring time 3 5)
+                     (substring time 2 4)))
+
+;;; To be moved to a different location later
+(define (add-leading-zero-if-less-than-ten num)
+  (if (< num 10) (string-append "0" (number->string num)) (number->string num)))
+
+(define (get-display-hour time)
+  (add-leading-zero-if-less-than-ten (get-twelve-hour-time (get-hour-from-time time))))
+
+(define (twentyfour-to-twelve-hour-time time)
+  (string-append* (list
+                   (get-display-hour time) ":" (get-display-minutes time)
+                   (get-time-period (get-hour-from-time time)))))
